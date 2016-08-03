ff1e4581e4bd6bb0b55be1344c56ac749a486466
Brian-It <1183561811@qq.com>
2016-08-03 17:46:34 +0800

    update test-kvm.sh
    modify

--- common/scripts/kvm/test-kvm.sh
+++ common/scripts/kvm/test-kvm.sh
@@ -58,17 +58,17 @@ tamper_guest()
 #!/bin/sh
     exec > /root/guest.log 2>&1
     echo "$PREFIX_KVM_BOOT 0 pc pass"
-    ping -w 20 -c 10 10.0.0.1 && echo "$PREFIX_KVM_GUEST_NET 0 pc pass" || echo "$PREFIX_KVM_GUEST_NET 0 pc fail"
+    #ping -w 20 -c 10 10.0.0.1 && echo "$PREFIX_KVM_GUEST_NET 0 pc pass" || echo "$PREFIX_KVM_GUEST_NET 0 pc fail"
     sh $TEST_SCRIPT
 EOF
     chmod a+x /mnt/usr/bin/test-guest.sh
 
     umount /mnt
-    mount /dev/nbd0p1 /mnt/
+    # mount /dev/nbd0p1 /mnt/
     case $prefix in
         aarch64)
             cp Image /mnt
-            echo 'FS0:\Image root=/dev/vda2 rw rootwait mem=1024M earlyprintk=pl011,0x9000000 console=ttyAMA0,38400n8' > /mnt/startup.nsh
+            echo 'FS0:\Image root=/dev/vda2 rw rootwait mem=384M earlyprintk=pl011,0x9000000 console=ttyAMA0,38400n8' > /mnt/startup.nsh
             ;;
         armv7l)
             cp zImage-vexpress /mnt/zImage
@@ -77,7 +77,7 @@ EOF
     esac
     umount /mnt
     sync
-    qemu-nbd -d /dev/nbd0
+    # qemu-nbd -d /dev/nbd0
 
 }
 
@@ -126,6 +126,7 @@ KVM_HOST_NET="kvm-host-net-1:"
 KVM_GUEST_NET="kvm-guest-net-1:"
 KVM_INIT="kvm-init-1:"
 KVM_BOOT="kvm-boot-1:"
+DIR="/run/media/sda1"
 if [ "x$1" = "xbenchmark" ]; then
     KVM_HOST_NET="$KVM_HOST_NET 0 none"
     KVM_GUEST_NET="$KVM_GUEST_NET 0 none"
@@ -149,35 +150,37 @@ if [ $? = 2 ]; then
     EXTRACT_BUILD_NUMBER="curl -sk"
     DOWNLOAD_FILE="curl -SOkL -# "
 else
-    EXTRACT_BUILD_NUMBER="wget -q --no-check-certificate -O -"
-    DOWNLOAD_FILE="wget --no-clobber --progress=dot -e dotbytes=2M --no-check-certificate"
+    EXTRACT_BUILD_NUMBER="wget -O -"
+    DOWNLOAD_FILE="wget "
 fi
 
-BUILD_NUMBER_GUEST=`$(echo $EXTRACT_BUILD_NUMBER) https://ci.linaro.org/job/kvm-guest-image/lastSuccessfulBuild/buildNumber`
-BUILD_NUMBER_HOST=`$(echo $EXTRACT_BUILD_NUMBER) https://ci.linaro.org/job/linux-kvm/lastSuccessfulBuild/buildNumber`
+# BUILD_NUMBER_GUEST=`$(echo $EXTRACT_BUILD_NUMBER) https://ci.linaro.org/job/kvm-guest-image/lastSuccessfulBuild/buildNumber`
+# BUILD_NUMBER_HOST=`$(echo $EXTRACT_BUILD_NUMBER) https://ci.linaro.org/job/linux-kvm/lastSuccessfulBuild/buildNumber`
 
-$DOWNLOAD_FILE http://snapshots.linaro.org/ubuntu/images/kvm-guest/$BUILD_NUMBER_GUEST/armhf/kvm-armhf.qcow2.xz
-$DOWNLOAD_FILE http://snapshots.linaro.org/ubuntu/images/kvm/arndale/$BUILD_NUMBER_HOST/zImage-armv7
-mv zImage-armv7 zImage-vexpress
-$DOWNLOAD_FILE http://snapshots.linaro.org/ubuntu/images/kvm/arndale/$BUILD_NUMBER_HOST/vexpress-v2p-ca15-tc1.dtb
-
-xz -d kvm-armhf.qcow2.xz
 
 case ${ARCH} in
     armv7l)
+	    
+		$DOWNLOAD_FILE http://snapshots.linaro.org/ubuntu/images/kvm-guest/$BUILD_NUMBER_GUEST/armhf/kvm-armhf.qcow2.xz
+		$DOWNLOAD_FILE http://snapshots.linaro.org/ubuntu/images/kvm/arndale/$BUILD_NUMBER_HOST/zImage-armv7
+		mv zImage-armv7 zImage-vexpress
+		$DOWNLOAD_FILE http://snapshots.linaro.org/ubuntu/images/kvm/arndale/$BUILD_NUMBER_HOST/vexpress-v2p-ca15-tc1.dtb
+		xz -d kvm-armhf.qcow2.xz
         modprobe nbd max_part=16
         ;;
     aarch64)
-        hwpack=`uname -r|sed -e's,.*-,,'`
-        $DOWNLOAD_FILE http://snapshots.linaro.org/ubuntu/images/kvm-guest/$BUILD_NUMBER_GUEST/arm64/kvm-arm64.qcow2.xz
-        $DOWNLOAD_FILE http://snapshots.linaro.org/ubuntu/images/kvm/$hwpack/$BUILD_NUMBER_HOST/Image-${hwpack}
-        $DOWNLOAD_FILE http://snapshots.linaro.org/ubuntu/images/kvm/$hwpack/$BUILD_NUMBER_HOST/nbd-${hwpack}.ko.gz
-        $DOWNLOAD_FILE http://releases.linaro.org/components/kernel/uefi-linaro/15.12/release/qemu64/QEMU_EFI.fd
-        xz -d kvm-arm64.qcow2.xz
-        zcat nbd-${hwpack}.ko.gz > nbd.ko
-        insmod nbd.ko max_part=16
-        mv Image-${hwpack} Image
-        tamper_guest kvm-arm64.qcow2 aarch64
+        # hwpack=`uname -r|sed -e's,.*-,,'`
+        # $DOWNLOAD_FILE http://snapshots.linaro.org/ubuntu/images/kvm-guest/$BUILD_NUMBER_GUEST/arm64/kvm-arm64.qcow2.xz
+        # $DOWNLOAD_FILE http://snapshots.linaro.org/ubuntu/images/kvm/$hwpack/$BUILD_NUMBER_HOST/Image-${hwpack}
+        # $DOWNLOAD_FILE http://snapshots.linaro.org/ubuntu/images/kvm/$hwpack/$BUILD_NUMBER_HOST/nbd-${hwpack}.ko.gz
+        # $DOWNLOAD_FILE http://releases.linaro.org/components/kernel/uefi-linaro/15.12/release/qemu64/QEMU_EFI.fd
+        # xz -d kvm-arm64.qcow2.xz
+		$DOWNLOAD_FILE  -P $DIR http://10.192.208.75/lars_logs/b59118/images/ls1012ardb/nbd.ko
+        # zcat nbd-${hwpack}.ko.gz > nbd.ko
+        insmod $DIR/nbd.ko max_part=16
+        # mv Image-${hwpack} Image
+		$DOWNLOAD_FILE  -P $DIR http://10.192.208.75/lars_logs/b59118/images/ls1012ardb/kvm-arm64.qcow2
+        tamper_guest $DIR/kvm-arm64.qcow2 aarch64
         ;;
     *)
         echo unknown arch ${ARCH}
